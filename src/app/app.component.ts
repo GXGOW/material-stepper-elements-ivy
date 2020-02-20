60491fbf3d495de64ace0abbc2049e9a1b141609
Nicolas Loots <nicolas.loots@outlook.be>
2020-02-20 14:40:03 +0100

    feat: add lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit() {
+    console.log('init');
+  }
+}
